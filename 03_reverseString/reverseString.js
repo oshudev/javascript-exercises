4a9b83b3e1dd3b0630ca987b93c96f9dfa2265fb
oshudev <99813212+eurus1108@users.noreply.github.com>
2024-06-08 05:23:09 +0800

    Exercise 3 solution

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,11 @@
-const reverseString = function() {
+const reverseString = function (phrase) {
+    let reverseString = "";
 
+    for (let i = phrase.length - 1; i >= 0; i--) {
+        reverseString += phrase.charAt(i);
+    }
+
+    return reverseString;
 };
 
 // Do not edit below this line
