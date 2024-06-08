9a1be2e0de577261a529092100bf71af2fa30905
oshudev <99813212+eurus1108@users.noreply.github.com>
2024-06-08 20:39:28 +0800

    Exercise 9 solution

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,14 @@
-const palindromes = function () {
+const palindromes = function (word) {
+    const alphanumerical = "abcdefghijklmnopqrstuvwxyz0123456789";
 
+    const cleanedString = word
+        .toLowerCase()
+        .split("")
+        .filter((char) => alphanumerical.includes(char))
+        .join("");
+    const reversed = cleanedString.split("").reverse().join("");
+    console.log(cleanedString);
+    return cleanedString === reversed;
 };
 
 // Do not edit below this line
