5f9ae045e4e9e2b680f48b454fa0df2cc9e422c1
oshudev <99813212+eurus1108@users.noreply.github.com>
2024-06-08 21:43:24 +0800

    Exercise 10 solution

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,14 @@
-const fibonacci = function() {
+const fibonacci = function (num) {
+    let count = typeof num !== "number" ? parseInt(num) : num;
+    if (num < 0) return "OOPS";
 
+    let fib = [0, 1];
+
+    for (let i = 2; i <= count; i++) {
+        fib.push(fib[i - 1] + fib[i - 2]);
+    }
+
+    return fib[count];
 };
 
 // Do not edit below this line
